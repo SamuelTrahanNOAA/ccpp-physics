e7a6fc2f67d6956dd82d172d66ed38eb9589bd24
samuel.trahan <Samuel.Trahan@noaa.gov>
2021-08-27 20:14:12 +0000

    changes to avoid crashes

--- smoke/rrfs_smoke_wrapper.F90
+++ smoke/rrfs_smoke_wrapper.F90
@@ -399,7 +399,7 @@ contains
     do i=its,ite
       wdgust(i)=sqrt(u10m(i)**2+v10m(i)**2)
       snoweq(i)=max((25.-snow(i)*1000.)/25.,0.)
-      hwp(i)=44.09*wdgust(i)**1.82*(t2m(i)-dpt2m(i))**0.61*(1.-smc(i,1))**14.0*snoweq(i)*sigmaf(i)
+      !hwp(i)=44.09*wdgust(i)**1.82*(t2m(i)-dpt2m(i))**0.61*(1.-smc(i,1))**14.0*snoweq(i)*sigmaf(i)
     enddo
     
 !-------------------------------------
